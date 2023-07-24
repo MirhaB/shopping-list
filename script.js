815b74075b693f7c638bc61cd95a5ce9c3f4d8ee
mirhaB <mirha.babic@orbicode.com>
2023-07-24 13:26:55 +0200

    Init commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+
+
+itemForm.addEventListener('submit', addItem);
+
+function addItem(e) {
+ e.preventDefault();
+
+ if(itemInput.value === '')
+ alert('Unesite item');
+
+ const li = document.createElement('li');
+
+ li.appendChild(document.createTextNode(itemInput.value));
+const button = createButton('remove-item btn-link text-red');
+li.appendChild(button);
+itemList.appendChild(li); 
+}
+
+
+function createButton(classes) {
+    const button = document.createElement('button');
+    button.className = classes;
+
+    const icon = createIcon('fa-solid fa-xmark');
+    button.appendChild(icon);
+    return button;
+}
+
+function createIcon(classes) {
+    const icon = document.createElement('icon');
+    icon.className = classes;
+    return icon;
+}
